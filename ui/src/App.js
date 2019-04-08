2db1f9ccbb63634985e651a4ed7c7bfbddb1f6c7
Chamindra-Yapa <Chamindrak7@gmail.com>
2019-04-08 12:17:29 +0530

    changed image for mobile view

--- ui/src/App.js
+++ ui/src/App.js
@@ -53,7 +53,6 @@ class NewsDashboard extends React.Component{
                     adType={'SQUAREBOTTOM MEDIUM'}/>
                 </div> 
                );
-
         }
         else{
           return (
